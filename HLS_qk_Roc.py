20fd8cb8372a72bd747922cecc34f765f5187a77
Javier Duarte <jduarte@ucsd.edu>
2023-12-20 20:18:29 -0800

    fix agreement

--- HLS_qk_Roc.py
+++ HLS_qk_Roc.py
@@ -17,16 +17,16 @@ model = load_qmodel("qkL1JetTagModel.h5")
 
 #Conver model to HLS
 import hls4ml
-config = hls4ml.utils.config_from_keras_model(model, granularity='name')
+config = hls4ml.utils.config_from_keras_model(model, granularity='name', default_precision='fixed<16,6>')
 print("-----------------------------------")
 
+trace = False
 for layer in config['LayerName']:
-    config['LayerName'][layer]['Trace'] = True
+    config['LayerName'][layer]['Trace'] = trace
 config['LayerName']['q_conv1d']['ReuseFactor'] = 2
 config['LayerName']['q_conv1d_1']['ReuseFactor'] = 2
 config["LayerName"]["q_conv1d"]["ConvImplementation"] = "Pointwise"
 config["LayerName"]["q_conv1d_1"]["ConvImplementation"] = "Pointwise"
-config["LayerName"]["input_1"]["Precision"] = "fixed<10,3>"
 
 hls_model = hls4ml.converters.convert_from_keras_model(model,
                                                        hls_config=config,
@@ -35,6 +35,8 @@ hls_model = hls4ml.converters.convert_from_keras_model(model,
 
 #Compile model, no need to convert if we are plotting performance
 hls_model.compile()
+
+hls4ml.utils.plot_model(hls_model, show_shapes=True, show_precision=True, to_file='model_summary.png')
 
 # Handle Data:
 with h5py.File("/home/users/russelld/L1JetTagger/dataForgeScripts/newTestDataST30.h5", "r") as hf:
@@ -85,20 +87,20 @@ plt.xlabel('hls4ml')
 plt.ylabel('QKeras')
 plt.savefig("output_correlation.png", dpi=300)
 
+if trace: 
+    y_hls, hls4ml_trace = hls_model.trace(X_test)
+    keras_trace = hls4ml.model.profiling.get_ymodel_keras(model, X_test)
 
-y_hls, hls4ml_trace = hls_model.trace(X_test)
-keras_trace = hls4ml.model.profiling.get_ymodel_keras(model, X_test)
-
-for layer in hls4ml_trace.keys():
-    plt.figure()
-    if layer not in keras_trace: continue
-    plt.scatter(hls4ml_trace[layer].flatten(), keras_trace[layer].flatten(), s=0.2)
-    min_x = min(np.amin(hls4ml_trace[layer]), np.amin(keras_trace[layer]))
-    max_x = max(np.amax(hls4ml_trace[layer]), np.amax(keras_trace[layer]))
-    plt.plot([min_x, max_x], [min_x, max_x], c='gray')
-    plt.xlabel(f'hls4ml {layer}')
-    plt.ylabel(f'QKeras {layer}')
-    plt.savefig(f'profiling_{layer}.png', dpi=300)
+    for layer in hls4ml_trace.keys():
+        plt.figure()
+        if layer not in keras_trace: continue
+        plt.scatter(hls4ml_trace[layer].flatten(), keras_trace[layer].flatten(), s=0.2)
+        min_x = min(np.amin(hls4ml_trace[layer]), np.amin(keras_trace[layer]))
+        max_x = max(np.amax(hls4ml_trace[layer]), np.amax(keras_trace[layer]))
+        plt.plot([min_x, max_x], [min_x, max_x], c='gray')
+        plt.xlabel(f'hls4ml {layer}')
+        plt.ylabel(f'QKeras {layer}')
+        plt.savefig(f'profiling_{layer}.png', dpi=300)
 
 plt.figure()
 plt.plot(fpr_Ab_qkeras, tpr_Ab_qkeras, label=" qkeras AUC={:.3f}".format(auc_Ab_qkeras))
@@ -113,6 +115,3 @@ plt.legend(loc="best")
 plt.xscale("log")
 plt.grid(True)
 plt.savefig("hls_Qk_ROCCurve.png")
-
-hls4ml.utils.plot_model(hls_model, show_shapes=True, show_precision=True, to_file='model_summary.png')
-
--- hls4ml_prj/build_lib.sh
+++ hls4ml_prj/build_lib.sh
@@ -9,7 +9,7 @@ fi
 LDFLAGS=
 INCFLAGS="-Ifirmware/ap_types/"
 PROJECT=myproject
-LIB_STAMP=AECAd5Cc
+LIB_STAMP=ACDB342C
 
 ${CC} ${CFLAGS} ${INCFLAGS} -c firmware/${PROJECT}.cpp -o ${PROJECT}.o
 ${CC} ${CFLAGS} ${INCFLAGS} -c ${PROJECT}_bridge.cpp -o ${PROJECT}_bridge.o
